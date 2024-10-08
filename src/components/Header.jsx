1098c3c21830c3c192436b91045a0360a5d50cd5
adjay27 <adityawhyjayaa@gmail.com>
2024-10-08 13:49:56 +0700

    feat: add navbar section to UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import React from "react";
 import Navbar from "./Navbar";
 
@@ -7,16 +5,14 @@ const Header = () => {
     const [navOpen, setNavOpen] = React.useState(false);
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid-cols-[1fr,3fr,1fr] ">
                 <h1 className="text-white text-2xl font-bold">adjay27</h1>
-
-                <div className=" relative md:justify-self-center">
+                <div className="relative md:justify-self-center">
                     <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
                         <span className="material-symbols-rounded">
                             {navOpen ? "close" : "menu"}
                         </span>
                     </button>
-
                     <Navbar navOpen={navOpen} />
                 </div>
                 <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">Contact</a>
